6953a57c3d00e1d7bd103ca1448ad9f4b713c0ce
Aubrey <rouettom@gmail.com>
2023-05-26 15:55:14 +0200

    Update Party

--- src/types/main/Party.ts
+++ src/types/main/Party.ts
@@ -1,29 +1,81 @@
 import { randomUUID, UUID } from "crypto";
 
 import parties from "@parties";
+import User, { UserClientType } from "@User";
 
 export default class Party {
-  public static exists(id: UUID): boolean {
-    return parties[id] !== undefined;
-  }
-  public static get(id: UUID): Party {
-    return parties[id];
-  }
+	public static exists(id: UUID): boolean {
+		return parties[id] !== undefined;
+	}
+	public static get(id: UUID): Party {
+		return parties[id];
+	}
 
-  private readonly _id: UUID;
-  public get id(): UUID {
-    return this._id;
-  }
+	private readonly _id: UUID;
+	public get id(): UUID {
+		return this._id;
+	}
 
-  public constructor() {
-    do {
-      this._id = randomUUID();
-    } while (Party.exists(this._id));
+	private _desktopUser: Nullable<User> = null;
+	public get desktopUser(): Nullable<User> {
+		return this._desktopUser;
+	}
 
-    parties[this._id] = this;
-  }
+	private _mobileUser: Nullable<User> = null;
+	public get mobileUser(): Nullable<User> {
+		return this._mobileUser;
+	}
 
-  public destroy() {
-    delete parties[this._id];
-  }
+	public get connectedUsers(): User[] {
+		return [this._desktopUser, this._mobileUser].filter(user => user !== null) as User[];
+	}
+
+	public get connectedUsersAsClientTypes(): UserClientType[] {
+		const connectedUsersClientTypes = this.connectedUsers.map(user => user.clientType);
+		return connectedUsersClientTypes.filter(clientType => clientType !== null) as UserClientType[];
+	}
+
+	// TODO: Implement a more secure password system in the future.
+	private _password: Nullable<string> = null;
+
+	public constructor(password: Nullable<string> = null) {
+		do {
+			this._id = randomUUID();
+		} while (Party.exists(this._id));
+
+		parties[this._id] = this;
+
+		this._password = password;
+	}
+
+	public destroy() {
+		delete parties[this._id];
+	}
+
+	public addUser(user: User): boolean {
+		switch (user.clientType) {
+			case "Desktop": {
+				if (this._desktopUser !== null) {
+					return false;
+				}
+
+				this._desktopUser = user;
+				return true;
+			}
+			case "Mobile": {
+				if (this._mobileUser !== null) {
+					return false;
+				}
+
+				this._mobileUser = user;
+				return true;
+			}
+			default:
+				return false;
+		}
+	}
+
+	public isCorrectPassword(password: string): boolean {
+		return this._password === password;
+	}
 }
