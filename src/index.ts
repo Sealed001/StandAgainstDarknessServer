e4ee3b41663d2140ff8c6b0ced57320436935f7f
Aubrey <rouettom@gmail.com>
2023-05-26 15:38:56 +0200

    Update index

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,20 @@ import CustomSocket from "@Socket";
 import CustomServer from "@Server";
 
 import serverOptions from "./serverOptions";
-import setupInitializationEvents from "@socket/initialization/setupInitializationEvents";
+
+import setupInitializationEvents from "./utils/setupInitializationEvents";
+import removeInitializationEvents from "./utils/removeInitializationEvents";
 
 const io: CustomServer = new Server(serverOptions);
+const port: number = !!process.env.PORT ? Number(process.env.PORT) : 3000;
 
-io.on('connection', (socket: CustomSocket) => {
-  setupInitializationEvents(socket);
-  socket.removeListener('askClientIdentity')
-  socket.once('init', (d) => {
-
-  });
-
-  for (const evName of eventsToRelay) {
-    socket.on(evName, () => {
-
-    });
-  }
-
+io.on("connection", (socket: CustomSocket) => {
+	setupInitializationEvents(socket)
+		.then(() => {
+			removeInitializationEvents(socket);
+		})
+		.catch(() => {})
+		.finally(() => {});
 });
 
-io.listen(3000);
+io.listen(port);
