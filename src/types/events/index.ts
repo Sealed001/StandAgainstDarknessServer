94e56a5a400be99a01c420bd23135280b99ef900
Aubrey <rouettom@gmail.com>
2023-05-27 03:20:59 +0200

    Add index file for events folder

---
+++ src/types/events/index.ts
@@ -0,0 +1,5 @@
+import ClientToServerEvents from "./ClientToServerEvents";
+import ServerToClientEvents from "./ServerToClientEvents";
+import InterServerEvents from "./InterServerEvents";
+
+export { ClientToServerEvents, ServerToClientEvents, InterServerEvents };
