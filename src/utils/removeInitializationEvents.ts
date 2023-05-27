a6d394b9e8a9edd01ae5b065700d47f218652563
Aubrey <rouettom@gmail.com>
2023-05-27 16:35:47 +0200

    Add removeInitializationEvents

--- src/utils/removeInitializationEvents.ts
+++ src/utils/removeInitializationEvents.ts
@@ -1,5 +1,6 @@
 import { Socket } from "socket.io";
 
 export default function (socket: Socket) {
-
-};
+	socket.removeAllListeners("askClientIdentity");
+	socket.removeAllListeners("tellClientIdentity");
+}
