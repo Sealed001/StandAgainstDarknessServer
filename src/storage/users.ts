451ef9828f75468e26e6247cc4671081d0881394
Aubrey <rouettom@gmail.com>
2023-05-27 16:45:41 +0200

    Change identification strategy 2

--- src/storage/users.ts
+++ src/storage/users.ts
@@ -2,4 +2,23 @@ import { UUID } from "crypto";
 import User from "@User";
 
 const users: Record<UUID, User> = {};
+export const usersCount = {
+	value: 0,
+	increment() {
+		++this.value;
+	},
+	decrement() {
+		--this.value;
+	},
+};
+
+export function addUser(user: User) {
+	users[user.id] = user;
+	usersCount.increment();
+}
+export function removeUser(user: User) {
+	delete users[user.id];
+	usersCount.decrement();
+}
+
 export default users;
--- src/utils/setupInitializationEvents.ts
+++ src/utils/setupInitializationEvents.ts
@@ -1,26 +1,79 @@
-import { UUID } from "crypto";
-
 import Socket from "@Socket";
 import User from "@User";
 
+import { usersCount } from "@users";
+
+import config from "@config";
+
 export default function (socket: Socket) {
 	return new Promise<void>((resolve, reject) => {
-		socket.once("askClientIdentity", () => {
-			const user = new User();
-			user.bindSocket(socket);
+		socket.once("askClientIdentity", data => {
+			if (usersCount.value >= config.userMaxCount) {
+				socket.emit("askClientIdentityResponse", {
+					success: false,
+					error: "SERVER_FULL",
+				});
 
-			socket.emit("tellClientIdentity", user.id);
+				reject();
+				return;
+			}
+
+			if (
+				data.clientType !== "Desktop" &&
+				data.clientType !== "Mobile"
+			) {
+				socket.emit("askClientIdentityResponse", {
+					success: false,
+					error: "WRONG_CLIENT_TYPE",
+				});
+
+				reject();
+				return;
+			}
+
+			const user = new User()
+				.setClientType(data.clientType)
+				.bindSocket(socket);
+
+			socket.emit("askClientIdentityResponse", {
+				success: true,
+				id: user.id,
+			});
+
 			resolve();
 		});
-		socket.once("tellClientIdentity", (id: UUID) => {
-			if (User.exists(id)) {
-				const user = User.get(id);
-				user.bindSocket(socket);
+		socket.once("tellClientIdentity", data => {
+			const id = data.id;
 
-				resolve();
-			} else {
+			if (typeof id !== "string") {
+				socket.emit("tellClientIdentityResponse", {
+					success: false,
+					error: "WRONG_CLIENT_ID_FORMAT",
+				});
+
 				reject();
+				return;
 			}
+
+			if (!User.exists(id)) {
+				socket.emit("tellClientIdentityResponse", {
+					success: false,
+					error: "CLIENT_ID_NOT_FOUND",
+				});
+
+				reject();
+				return;
+			}
+
+			const user = User.get(id);
+
+			user.bindSocket(socket);
+
+			socket.emit("tellClientIdentityResponse", {
+				success: true,
+			});
+
+			resolve();
 		});
 	});
 }
