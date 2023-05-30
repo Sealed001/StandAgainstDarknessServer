36b9286f508a5e25fb6bdeb304abbc47a390bfd6
Aubrey <rouettom@gmail.com>
2023-05-31 00:06:21 +0200

    Add Party name and password size range to config file

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,14 @@ const debug = process.env.NODE_ENV !== "production";
 
 type Configuration = {
 	userMaxCount: number;
+	partyNameSizeRange: [number, number];
+	partyPasswordSizeRange: [number, number];
 	debug?: boolean;
 };
 
 const config: Configuration = {
+	partyNameSizeRange: [3, 35],
+	partyPasswordSizeRange: [3, 20],
 	userMaxCount: 100,
 	debug,
 };
